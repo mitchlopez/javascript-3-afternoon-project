e09ca2d96fc4fd90e48d41837a9b6e34754f13ce
Mitchell Lopez <mitchellslopez@gmail.com>
2019-06-22 21:12:18 -0500

    Completed assignment

--- spread.js
+++ spread.js
@@ -17,8 +17,8 @@ let product = {
   imageUrl: 'https://i.ebayimg.com/images/g/9T4AAOSwbmdZf2jN/s-l640.jpg'
 }
 
-// code here
-
+let productCopy = {...product};
+// console.log(productCopy);
 
 
 
@@ -28,7 +28,8 @@ let product = {
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+let colorsCopy = [...colors]
+
 
 
 
@@ -43,7 +44,7 @@ function addNums(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-// code here
+let result = addNums(...numbers);
 
 
 ////////// PROBLEM 4 //////////
@@ -53,4 +54,5 @@ function addNums(num1, num2, num3) {
 let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
-// code here
+let combinedArrays = [
+...arr1, ...arr2];
